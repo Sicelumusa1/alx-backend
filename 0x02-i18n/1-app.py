cf5bfee47232290d9ad0daef9e8912e6ae476e9f
Sicelumusa <musaqwabe@gmail.com>
2024-04-09 13:19:20 +0000

    added file 1 & 2

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,8 +5,19 @@ Setup a basic Flask app
 """
 
 from flask import Flask, render_template
+from flask_babel import Babel
 
 app = Flask(__name__)
+
+class Config:
+    """
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE ='en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+babel = Babel(app)
+app.config.from_object(Config)
 
 @app.route("/")
 def index():
---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+"""
+Setup a basic Flask app
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+class Config:
+    """
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE ='en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+babel = Babel(app)
+app.config.from_object(Config)
+
+@babel.localeselector
+def get_locale():
+    """
+    """
+    user_languages = request.accept_languages
+    for lang, _ in user_languages:
+        if lang in ap.config['LANGUAGES']:
+            rturn lang
+    return app.config['BABEL_DEFAULT_LOCALE']
+
+@app.route("/")
+def index():
+    """
+    renders index.html
+    """
+    return render_template('index.html')
